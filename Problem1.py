e15d99f3a045e12da7e6cd1255ab6d55ffc07bc5
jlpusher <jlpusher@unomaha.edu>
2025-05-08 17:48:46 +0000

    video updates

--- Problem1.py
+++ Problem1.py
@@ -1,11 +1,11 @@
 #Project Euler Problem 1
 
-import NumberTests
+import NumberTests  #import from external file
 
 def main():
   total = 0
-  for i in range(1001):
-    if NumberTests.isThreeOrFive(i):
+  for i in range(1001):   #going through all the numbers up through 1000
+    if NumberTests.isThreeOrFive(i):  #if number is 3 or 5 then we add to total
       total += i
 
   print(total)
