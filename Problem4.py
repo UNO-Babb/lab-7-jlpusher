1e981c909c5d36de55f8e25e98fcd965f780c227
jlpusher <jlpusher@unomaha.edu>
2025-05-09 16:33:39 +0000

    saving as is stuck on prob 4

--- Problem4.py
+++ Problem4.py
@@ -8,15 +8,17 @@ for i in range(100,1000):
     for j in range(100,1000):
         productsOfThree.append(i*j)
 
-productPalindrome3 = []
+
 
 def main():
+  productPalindrome3 = []
+
   for p in productsOfThree:
-    if isPalindrome(p):
+    if isPalindrome(p):   
       productPalindrome3.append(p)
-      
-    largestPalindrome = max(productPalindrome3)
-    print(largestPalindrome)
+      largestPalindrome = max(productPalindrome3)
+      print(largestPalindrome)
+      #I can't get this to NOT print several values. This is due to being within the loop but if I pull it out then I get an error because the 'argument is empty'.
   
 
 if __name__ == '__main__':
